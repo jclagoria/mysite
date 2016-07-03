3bd2511c09a7b804aff8c47ece756ba241a328e4
jclagoria <Shenkun1979>
2016-07-03 01:42:16 -0300

    add filters, post and more

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,6 +122,6 @@ STATIC_URL = '/static/'
 # Configuration for emial sends
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'juankalagoria@gmail.com'
-EMAIL_HOST_PASSWORD = 'Shenkun2016'
+EMAIL_HOST_PASSWORD = '*******'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
