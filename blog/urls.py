2358562ea38bf3dc52690b0473bb3032ca8f8e42
jclagoria <Shenkun1979>
2016-07-03 01:41:15 -0300

    add filters, post and more

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
         name='post_detail'),
     # url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
     url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
+    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+
 ]
