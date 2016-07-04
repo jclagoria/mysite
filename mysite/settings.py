6ff54627938591d9190046adb906130a3b9821e5
jclagoria <Shenkun1979>
2016-07-04 00:49:13 -0300

    add sitemaps for de blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE_CLASSES = [
